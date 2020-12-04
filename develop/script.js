d35dcb7ae661b6089cc9b0b407adb400a496a237
Charles Latrobe <Charlie.bateman117@gmail.com>
2020-12-04 14:08:29 -0800

    add pseudocode for project, add basic functions for application.

---
+++ develop/script.js
@@ -0,0 +1,23 @@
+// create Elements
+// add event listeners
+// create variables for necessary elements
+// code relevant dom maniulation
+// add library to track date
+// set local storage for schedule
+
+generateCalendar()
+listenForEvents()
+setToStorage()
+
+function generateCalendar() {
+    console.log('generating calendar');
+}
+
+function listenForEvents () {
+    console.log('listening for events');
+}
+
+function setToStorage () {
+    console.log('setting to local storage');
+}
+
